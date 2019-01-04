153fd0fd16d03e6c38c3b5b0e8b74fdabde4b59f
aviator <nicholas_wolf@gmx.de>
2019-01-04 09:30:27 +0100

    translated comments

--- example/simpleCExample.c
+++ example/simpleCExample.c
@@ -8,13 +8,13 @@ void f(char* string){
 
 int main(void) {
 
-//handle für das sammeln der Testergebnisse
+//handle to collect the results
 
 	handle* handle_ = getEmptyHandle(&f);
 
 	generateHeader(handle_);
 
-//die tests durchführen
+//exceute some tests
 
 	if(assert_IntEqual(42, 43, "F1: 42 != 43", handle_)){
 		appendNote("Zusätzliche Debugginginformationen", handle_);
@@ -31,18 +31,17 @@ int main(void) {
 	int a[] = { 1, 2, 3 };
 	int b[] = { 2, 3, 4 };
 
-	//int f = assert_IntArrayEqual(a, b, 3, "F7: Arrays auf dem Bereich [0..3] nicht gleich!", handle_);//1
-
 	assert_IntInRange(2,1,5,"Nicht im Bereich1.",handle_);
 	assert_IntInRange(2,5,6,"Nicht im Bereich2.",handle_);
 
-//Siehe Abschnitt über Weitere Features.
+//See section about additional functions.
 
 	appendNote("Einfache Meldung anzeigen.", handle_);
 	appendNote("Ggf. um Programmverlauf zu folgen.", handle_);
 	appendNoteAndMessage("Ausgabe eines Strings zusätzlich zur Notiz","Ich bin der Inhalt.", handle_);
 	appendNoteAndInt("Ausgabe eines Ints",42, handle_);
-//Ausgabe erzeugen
+
+//show results
 
 
 	generateReport(handle_);
